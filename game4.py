d87ccbc0975508f7e17ad6c0925ceb839ff80940
coru2com <47563036+coru2com@users.noreply.github.com>
2025-04-21 11:41:43 +0900

    Update game4.py

--- game4.py
+++ game4.py
@@ -3,6 +3,8 @@ import json
 
 class App:
     def __init__(self):
+        pyxel.init(160, 120, title="8bit BGM Player")
+
         # JSONから音楽データ読み込み
         with open("music.json", "rt") as f:
             self.music = json.load(f)
@@ -11,7 +13,7 @@ class App:
         self.play_music_on_startup()  # 起動時に再生！
 
         pyxel.run(self.update, self.draw)
-        
+
     def play_music_on_startup(self):
         for ch, sfx in enumerate(self.music):
             pyxel.sound(ch).set(*sfx)
@@ -19,8 +21,19 @@ class App:
         self.playing = True
 
     def update(self):
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            if self.playing:
+                pyxel.stop()
+                self.playing = False
+            else:
+                self.play_music_on_startup()
+
+        if pyxel.btnp(pyxel.KEY_ESCAPE):
+            pyxel.quit()
 
     def draw(self):
         pyxel.cls(0)
+        pyxel.text(30, 50, "[SPACE] Play / Stop", 7)
+        pyxel.text(30, 60, "[ESC] Exit", 7)
 
 App()
