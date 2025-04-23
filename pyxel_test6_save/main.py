aaac6211b3582b0bb32dfe61319b2d53273af13f
coru2com <47563036+coru2com@users.noreply.github.com>
2025-04-23 21:28:29 +0900

    Update main.py

--- pyxel_test6_save/main.py
+++ pyxel_test6_save/main.py
@@ -7,19 +7,10 @@ class App:
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        if pyxel.btnp(pyxel.KEY_SPACE):
-            self.score += 1
-
-        if pyxel.btnp(pyxel.KEY_S):
-            print(f"SAVE:{self.score}")  # JavaScript側で拾う用のセーブ出力
-
-        if pyxel.btnp(pyxel.KEY_L):
-            print("LOAD")  # JavaScript側がローカルストレージから読み込んで渡してくる
+        pass
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.text(10, 10, f"Score: {self.score}", 7)
-        pyxel.text(10, 30, "SPACE = +1", 6)
-        pyxel.text(10, 40, "S = Save / L = Load", 6)
+        pyxel.text(10, 10, f"Hello!", 7)
 
 App()
